247cf3fd4bddf64c111cbf939df03671c66514fc
Jan Czech <czechumennakompie@gmail.com>
2022-02-08 00:09:31 +0100

    UserDto & first compile

--- ForeginClientsAPI/ForeginClientsAPI/Models/UserDto.cs
+++ ForeginClientsAPI/ForeginClientsAPI/Models/UserDto.cs
@@ -2,6 +2,8 @@
 {
     public class UserDto
     {
-        
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PeselHash { get; set; }
     }
 }
